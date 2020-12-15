114a6e40e08789fa1463f00761e98f91da27b4f6
funera1 <funeral7776@gmail.com>
2020-12-15 23:05:04 +0900

    msg

--- kanikani3/app/src/main/java/com/example/kanikani3/MainActivity.kt
+++ kanikani3/app/src/main/java/com/example/kanikani3/MainActivity.kt
@@ -8,27 +8,4 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
-    companion object {
-        val macAddr: String // macアドレス
-            get() {
-                try {
-                    val all: List<NetworkInterface> = Collections.list(NetworkInterface.getNetworkInterfaces())
-                    for (nif in all) {
-                        if (!nif.name.equals("wlan0", ignoreCase = true)) continue
-                        val macBytes = nif.hardwareAddress ?: return ""
-                        val res1 = StringBuilder()
-                        for (b in macBytes) {
-                            res1.append(String.format("%02X:", b))
-                        }
-                        if (res1.length > 0) {
-                            res1.deleteCharAt(res1.length - 1)
-                        }
-                        return res1.toString()
-                    }
-                } catch (ex: Exception) {
-                }
-                return "02:00:00:00:00:00"
-            }
-    }
 }
---
+++ kanikani3/app/src/main/java/com/example/kanikani3/repository/transData.kt
@@ -0,0 +1,15 @@
+package com.example.kanikani3.repository
+
+//import com.google.firebase.database.IgnoreExtraProperties //firebase
+/**
+ * 送信するデータモデルはここ！サーバーと通信するときに使うところ
+ * ・ユーザネーム
+ * ・ツイッターアカウント名（＠～～～～）
+ * ・趣味
+ * ・メッセージ
+ */
+//@IgnoreExtraProperties
+//data class User(
+//    var userName: String = "",
+//    var twitterName: String = ""
+//)
