c3834108a888b76a7c87dc009b0ba2b8a6de72ab
shinbo <67352454+sauruskp@users.noreply.github.com>
2020-12-17 16:33:38 +0900

    success user page

--- kanikani3/app/src/main/java/com/example/kanikani3/MainActivity.kt
+++ kanikani3/app/src/main/java/com/example/kanikani3/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.kanikani3
 
 //import android.R
 
+//import android.R
+import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
-import android.content.Context
+import android.content.SharedPreferences
 import android.os.Build
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
@@ -18,22 +23,41 @@ class MainActivity : AppCompatActivity() {
     // メンバー変数
     private var mBluetoothAdapter // BluetoothAdapter : Bluetooth処理で必要
             : BluetoothAdapter? = null
+    var pref: SharedPreferences? = null
+    var b = false
+    var n = 0
 
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        pref = PreferenceManager.getDefaultSharedPreferences(this)
+        b = pref.getBoolean("booldata", false)
+        n = pref.getInt("intdata", 0)
+
 
         // Bluetoothアダプタの取得
-        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        val bluetoothManager = getSystemService(BLUETOOTH_SERVICE) as BluetoothManager
         mBluetoothAdapter = bluetoothManager.adapter
         if (null == mBluetoothAdapter) {    // Android端末がBluetoothをサポートしていない
             Toast.makeText(this, R.string.bluetooth_is_not_supported, Toast.LENGTH_SHORT).show()
             finish() // アプリ終了宣言
             return
         }
-    }
 
+        val editText = findViewById<EditText>(R.id.edit_text)
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            Toast.makeText(this, editText.text.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+    override fun onResume() {
+        super.onResume()
+        val e = pref!!.edit()
+        e.putBoolean("booldata", b)
+        e.putInt("intdata", n)
+        e.commit()
+    }
 
     companion object {
         // 定数
@@ -59,5 +83,5 @@ class MainActivity : AppCompatActivity() {
                 }
                 return "02:00:00:00:00:00"
             }
-    }
+     }
 }
