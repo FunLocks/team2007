81c409ff3e0d737602832fa2ce1068c02a4d7e24
shinbo <67352454+sauruskp@users.noreply.github.com>
2020-12-18 02:28:30 +0900

    edittextbag

---
+++ kanikani3/app/src/main/java/com/example/kanikani3/NextActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kanikani3
+
+import android.content.Intent
+
+@Suppress("DEPRECATION")
+class NextActivity {
+    var intent = Intent.getIntent()
+    var gettext = intent.getStringArrayExtra("inputdata")
+
+
+}
